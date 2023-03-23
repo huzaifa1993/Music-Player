2e38eb286a5d4a506ba2843345090a529203121f
huzaifa1993 <huothyfayusuf1993@gmail.com>
2023-03-23 22:56:59 +0000

    functionality in progress bar

--- script.js
+++ script.js
@@ -85,26 +85,32 @@ function updateProgressBar(e) {
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
         const durationMinutes = Math.floor(duration / 60);
-        console.log('minutes', durationMinutes);
         let durationSeconds = Math.floor(duration % 60);
         if (durationSeconds < 10) {
             durationSeconds = `0${durationSeconds}`;
         }
-        console.log('seconds', durationSeconds);
         if (durationSeconds) {
             durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
         }
         const currentMinutes = Math.floor(currentTime / 60);
-        console.log('minutes', currentMinutes);
         let currentSeconds = Math.floor(currentTime % 60);
         if (currentSeconds < 10) {
             currentSeconds = `0${currentSeconds}`;
         }
-        console.log('seconds', currentSeconds);
         currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`
     }
 }
 
+function setProgressBar(e) {
+    console.log(e);
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration} = music;
+    music.currentTime = (clickX / width) * duration;
+}
+
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
